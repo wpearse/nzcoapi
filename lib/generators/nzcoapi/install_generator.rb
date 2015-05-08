2d8aa53d17fb0ea45dd05890711dbcf14b3460dd
Will Pearse <hello@wpearse.com>
2015-05-08 18:07:51 +1200

    Tidy indentation

--- lib/generators/nzcoapi/install_generator.rb
+++ lib/generators/nzcoapi/install_generator.rb
@@ -1,15 +1,20 @@
 module Nzcoapi
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
 
-      desc "Creates application.yml for your application"
-      class_option :orm
+    module Generators
 
-      def copy_initializer
-        template '../templates/application.yml', 'config/application.yml'
-      end
+        class InstallGenerator < Rails::Generators::Base
+
+            source_root File.expand_path("../templates", __FILE__)
+
+            desc "Creates application.yml for your application"
+            class_option :orm
+
+            def copy_initializer
+            template '../templates/application.yml', 'config/application.yml'
+            end
+
+        end
 
     end
-  end
+    
 end
--- lib/nzcoapi.rb
+++ lib/nzcoapi.rb
@@ -13,20 +13,20 @@ module Nzcoapi
 	include Nokogiri
 	include Figaro
 
-    @@server_name = "www.businessdata.govt.nz"
+	@@server_name = "www.businessdata.govt.nz"
 	@@access_key = YAML.load_file("config/application.yml")['nzcoapi_access_key']
 	@@secret_key = YAML.load_file("config/application.yml")['nzcoapi_secret_key']
 	@@timestamp = Time.now.rfc2822 + " GMT"
 
-    base_uri = "http://" + @@server_name
-    format :xml
+	base_uri = "http://" + @@server_name
+	format :xml
 
   	headers "Access" => @@access_key
   	headers 'Accept' => "application/xml"
 
 	def self.search_for_company(string)
 		@@timestamp = Time.now.rfc2822 + " GMT"
-        @@resource_name = "/data/app/ws/rest/companies/entity/search/v2.0/#{string}"
+		@@resource_name = "/data/app/ws/rest/companies/entity/search/v2.0/#{string}"
 
 		stringtosign = "GET" + "\n" + @@server_name + "\n" + @@resource_name + "\n" + @@timestamp + "\n" + @@access_key + "\n" + "application/xml" + "\n"
 
@@ -38,7 +38,7 @@ module Nzcoapi
 
 	def self.find_director(string)
 		@@timestamp = Time.now.rfc2822 + " GMT"
-        @@resource_name = "/data/app/ws/rest/companies/role/search/v1.0/size/200/type/DIR/#{string}"
+		@@resource_name = "/data/app/ws/rest/companies/role/search/v1.0/size/200/type/DIR/#{string}"
 
 		stringtosign = "GET" + "\n" + @@server_name + "\n" + @@resource_name + "\n" + @@timestamp + "\n" + @@access_key + "\n" + "application/xml" + "\n"
 
@@ -50,12 +50,12 @@ module Nzcoapi
 
 	def self.find_company(id)
 		@@timestamp = Time.now.rfc2822 + " GMT"
-        @@resource_name = "/data/app/ws/rest/companies/entity/v2.0/#{id}"
+		@@resource_name = "/data/app/ws/rest/companies/entity/v2.0/#{id}"
 
 		stringtosign = "GET" + "\n" + @@server_name + "\n" + @@resource_name + "\n" + @@timestamp + "\n" + @@access_key + "\n" + "application/xml" + "\n"
 
 		headers "Authorization" => @@access_key + ":" + Base64.strict_encode64(OpenSSL::HMAC.digest('sha256', @@secret_key, stringtosign))
-        headers "Timestamp" => @@timestamp
+		headers "Timestamp" => @@timestamp
 
 		Nokogiri::XML((get(@@resource_name).body), 'UTF-8')
 	end
